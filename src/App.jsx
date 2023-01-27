429b1d3daf78f46cd59fd4e7423025e1f4926fd4
Michael Carr <mcarr2468@gmail.com>
2023-01-27 12:34:04 -0600

    added the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
 import { useState } from 'react'
 
-import { useStandardContext } from './contexts/standard_context'
-import { useBlogContext } from './contexts/blog_context'
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+
+import PrivateRoutes from './utility/PrivateRoutes'
+
+// public route
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Author from './pages/Author'
+import Category from './pages/Category'
+import Post from './pages/Post'
+import Search from './pages/Search'
+
+
+import Error from './pages/Error'
+// private route
+import CreateAPost from './pages/CreateAPost'
+
 
 function App() {
-  const { testFunc2 } = useStandardContext()
-  const { testFunc3 } = useBlogContext()
-
-  testFunc2()
-  testFunc3()
 
   return (
-    <div className='text-2xl font-bold underline'>
-      <h2 className="m-4">before auth</h2>
+    <div>
+      <Router>
+        <Routes>
+          <Route element={<PrivateRoutes/>}>
+            <Route element={<CreateAPost/>} path="/createapost"/>
+          </Route>
+
+          <Route element={<Home/>} path="/"/>
+          <Route element={<Login/>} path="/login"/>
+          <Route element={<SignUp/>} path="/SignUp"/>
+
+          <Route element={<Author/>} path="/author"/>
+          <Route element={<Category/>} path="/category"/>
+          <Route element={<Post/>} path="/post"/>
+          <Route element={<Search/>} path="/search"/>
+
+
+          <Route element={<Error/>} path="*"/>
+
+        </Routes>
+      </Router>
     </div>
   )
 }
