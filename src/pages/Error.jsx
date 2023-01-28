8774b9d408d384012aaf58640f5938f85bf06725
Michael Carr <mcarr2468@gmail.com>
2023-01-27 21:33:49 -0600

    increase height of container from 50vh to 75vh

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div className="h-[50vh] flex flex-col justify-center items-center">
+    <div className="h-[75vh] flex flex-col justify-center items-center">
       <h1 className="text-3xl font-semibold">An Error occurred.</h1>
       <Link to="/">
         <button className="form-primary-btn" type="button">Go Home</button>
