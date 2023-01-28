a41ed3f457b05838be8b3e4f9ab6c811ea3d1051
Michael Carr <mcarr2468@gmail.com>
2023-01-27 22:13:16 -0600

    added styles and logic that gets the inputs for the login and signup forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const Login = () => {
+  const [loginStateData, setLoginStateData] = React.useState({
+    email:'',
+    password:'',
+  })
+
+  React.useEffect(() => {
+    console.log(loginStateData)
+  }, [loginStateData])
+
   return (
-    <div>Login</div>
+    <div className="h-[75vh] flex flex-col justify-center items-center">
+      <form className="form-card">
+        <div className="form-input-container">
+          <label className="form-label" htmlFor="login-email">Email Address</label>
+          <input className="form-input" name="login-email" id="login-email" type="email" onChange={(e) => setLoginStateData({...loginStateData, email:e.target.value})}/>
+        </div>
+        <div className="form-input-container">
+          <label className="form-label" htmlFor="login-password">Password</label>
+          <input className="form-input" name="login-password" id="login-password" type="password" onChange={(e) => setLoginStateData({...loginStateData, password:e.target.value})}/>
+        </div>
+        <button className="form-primary-btn" type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const SignUp = () => {
+  const [signUpStateData, setSignUpStateData] = React.useState({
+    email:'',
+    password:'',
+  })
+
+  React.useEffect(() => {
+    console.log(signUpStateData)
+  }, [signUpStateData])
+
   return (
-    <div>SignUp</div>
+    <div className="h-[75vh] flex flex-col justify-center items-center">
+      <form className="form-card">
+        <div className="form-input-container">
+          <label className="form-label" htmlFor="login-email">Email Address</label>
+          <input className="form-input" name="login-email" id="login-email" type="email" onChange={(e) => setSignUpStateData({...signUpStateData, email:e.target.value})}/>
+        </div>
+        <div className="form-input-container">
+          <label className="form-label" htmlFor="login-password">Password</label>
+          <input className="form-input" name="login-password" id="login-password" type="password" onChange={(e) => setSignUpStateData({...signUpStateData, password:e.target.value})}/>
+        </div>
+        <button className="form-primary-btn" type="submit">Sign Up</button>
+      </form>
+    </div>
   )
 }
 
