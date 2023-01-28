7fc00502dd69ecae3cb7683975cfd06f8c79823c
Michael Carr <mcarr2468@gmail.com>
2023-01-27 21:23:58 -0600

    did some basic style for the error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className="h-[50vh] flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-semibold">An Error occurred.</h1>
+      <Link to="/">
+        <button className="form-primary-btn" type="button">Go Home</button>
+      </Link>
+    </div>
   )
 }
 
