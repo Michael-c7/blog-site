f39e5876a82b89185bb64eb54da6ef68b1a1cb41
Michael Carr <mcarr2468@gmail.com>
2023-02-19 18:13:35 -0600

    takes and shows  custom colors from the props

--- src/components/widgets/Tag.jsx
+++ src/components/widgets/Tag.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom"
 
 const Tag = (props) => {
   const { bgColor, link, text } = props
-  console.log(props)
+  
   return (
-    <Link to={link} className={`${bgColor ? `bg-[${bgColor}]` : "bg-slate-500"} text-xs py-1 px-4 font-medium rounded uppercase text-white text-center`}>
+    <Link to={link} className={`text-xs py-1 px-4 font-medium rounded uppercase text-white text-center`} style={{backgroundColor:`${bgColor}`}}>
     {text}
     </Link>
   )
