8a2b96544f4c1223617efe552fe394d8f65488e8
Michael Carr <mcarr2468@gmail.com>
2023-02-19 18:28:09 -0600

    replaced lorem ipsum text with the textText component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+
+import HeroSlider from '../components/HeroSlider'
+
+
+import TestText from '../components/TestText';
 
 const Home = () => {
   return (
-    // <div>Home</div>
-    <div className=' w-2/4'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt blanditiis reiciendis dolore sunt, eum ducimus doloribus consequuntur earum tempore ea? Dolorum magni quae maxime, saepe nostrum facilis impedit debitis dignissimos!
-    
+    <div>
+      <TestText/>
     </div>
-
   )
 }
 
