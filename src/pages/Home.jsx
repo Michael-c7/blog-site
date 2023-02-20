a0bd7981f55758fef1306204244204f770f81b15
Michael Carr <mcarr2468@gmail.com>
2023-02-20 12:11:51 -0600

    added the hero slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,10 @@ import React, { useState, useEffect, useRef } from 'react'
 
 import HeroSlider from '../components/HeroSlider'
 
-
-import TestText from '../components/TestText';
-
 const Home = () => {
   return (
     <div>
-      <TestText/>
+      <HeroSlider/>
     </div>
   )
 }
