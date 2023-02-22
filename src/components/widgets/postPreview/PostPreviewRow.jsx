ef74b794cac535cfc64c792fdc3a18dafcc4b571
Michael Carr <mcarr2468@gmail.com>
2023-02-22 13:28:18 -0600

    did the styles

--- src/components/widgets/postPreview/PostPreviewRow.jsx
+++ src/components/widgets/postPreview/PostPreviewRow.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from "react"
+import Tag from "../Tag"
+import Date from "../Date"
+import testImg from "../../../assets/images/testImg1.jpg"
+import { Link } from "react-router-dom"
 
 /*
 Where image is on the left
@@ -8,10 +12,27 @@ are on the right
 have a reverse prop for reverse needs eg:recent posts
 */
 
-
-const PostPreviewRow = () => {
+/**
+ * 
+ * @param {*} props 
+ * @param {string} direction - the flex direction 
+ * @returns 
+ */
+const PostPreviewRow = (props, direction) => {
   return (
-    <div>postPreviewRow</div>
+    <div className="flex items-center">
+        <Link className="min-[995px]:w-1/2 w-24 h-24" to="/link to post">
+          <img src={testImg} alt={"alt text here"} title={`alt text here`} className="w-full h-full rounded-xl object-cover"/>
+        </Link>
+
+      <div className="ml-4">
+        <Tag {...{bgColor:"#ccc", link:"/test", text:"tag text"}}/>
+        <Link to="/link to post here">
+          <h2 className="font-bold text-black my-1 leading-snug	">The Great Time For Enjoy City View On Mountain</h2>
+        </Link>
+        <Date/>
+      </div>
+    </div>
   )
 }
 
--- src/components/widgets/postPreview/PostPreviewTogether.jsx
+++ src/components/widgets/postPreview/PostPreviewTogether.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import Tag from "../Tag"
+import AuthorAndDate from "../AuthorAndDate"
+
+import testImg from "../../../assets/images/testImg1.jpg"
+
 
 /*
 Where the text is on to of the image,
@@ -9,7 +14,16 @@ eg: in example Top of the week section
 
 const PostPreviewTogether = () => {
   return (
-    <div>PostPreviewTogether</div>
+    <section className='relative'>
+      <img src={testImg} alt="the alt text here" title="alt text here" className='w-full h-full rounded-xl'/>
+      <div className="absolute top-0 m-6">
+        <Tag {...{bgColor:"#ccc", link:"/test", text:"tag text"}}/>
+      </div>
+      <div className='absolute bottom-0 m-6'>
+        <h2 className='text-white font-medium text-2xl'>the title goes here</h2>
+        <AuthorAndDate {...{textColor:"#fff"}}/>
+      </div>
+    </section>
   )
 }
 
