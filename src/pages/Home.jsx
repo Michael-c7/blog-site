96558b7b5a2c0eb00b1af4ad36cc9dc62cb4b304
Michael Carr <mcarr2468@gmail.com>
2023-02-22 13:30:03 -0600

    added top of the week section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect, useRef } from 'react'
 
+
+// components
 import HeroSlider from '../components/HeroSlider'
+import EditorsChoice from '../components/EditorsChoice'
+import TopOfTheWeek from '../components/TopOfTheWeek'
 
 const Home = () => {
   return (
-    <div>
+    <div className='outer-width mx-auto'>
       <HeroSlider/>
+      <EditorsChoice/>
+      <TopOfTheWeek/>
     </div>
   )
 }
