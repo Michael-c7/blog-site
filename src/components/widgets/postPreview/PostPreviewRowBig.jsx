e3aa9e7810ed668db5a4d06a814512d96d4e9daa
Michael Carr <mcarr2468@gmail.com>
2023-02-24 12:42:58 -0600

    moved the vertical spacing out of the postPreviewRowBig component and into a container div in the moreArticles component

--- src/components/widgets/postPreview/PostPreviewRowBig.jsx
+++ src/components/widgets/postPreview/PostPreviewRowBig.jsx
@@ -1,12 +1,12 @@
-import React from "react"
+import React from 'react'
 
 import testImg from "../../../assets/images/testImg1.jpg"
 import { FaComment } from "react-icons/fa"
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 // components
-import Tag from "../Tag"
-import Date from "../Date"
+import Tag from '../Tag'
+import Date from '../Date'
 
 const PostPreviewRowBig = (props) => {
   /* The articleTextCutoff number is based on how good
@@ -15,25 +15,25 @@ const PostPreviewRowBig = (props) => {
   let testText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
   return (
-    <section className={`flex ${props.direction ? props.direction : "flex-row"} gap-6 my-8 first-of-type:mt-0 last-of-type:mb-0`}>
-        <Link to="/link to post">
-            <img src={testImg} alt="alt text here" className="w-full rounded-lg"/>
+    <section className={`flex ${props.direction ? props.direction : "flex-row"} gap-6`}>
+        <Link to="/link to post" className=''>
+            <img src={testImg} alt="alt text here" className='w-full rounded-lg'/>
         </Link>
         <div>
-            <header className="flex items-center">
+            <header className='flex items-center'>
                 <Tag {...{bgColor:"#ccc", link:"/tag link here", text:"text here"}}/>
-                <div className="mx-2">
+                <div className='mx-2'>
                     <Date/>
                 </div>
-                <div className="relative flex items-center text-gray-500">
-                    <div className="relative">
-                        <FaComment className="text-xs mr-1"/>
+                <div className='relative flex items-center text-gray-500'>
+                    <div className='relative'>
+                        <FaComment className='text-xs mr-1'/>
                     </div>
-                    <div className="relative top-[-1px]">0</div>
+                    <div className='relative top-[-1px]'>0</div>
                 </div>
             </header>
             <Link to="/link to post here">
-                <h2 className="text-2xl font-medium my-2">article title  goes here</h2>
+                <h2 className='text-2xl font-medium my-2'>article title  goes here</h2>
             </Link>
             <p>{testText.length > articleTextCutoff ? `${testText.slice(0, articleTextCutoff)}...` : testText}</p>
         </div>
