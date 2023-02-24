6ce316362f148c875bdb88384b22d76c4e253783
Michael Carr <mcarr2468@gmail.com>
2023-02-23 21:26:36 -0600

    did the styles

---
+++ src/components/MoreArticles.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PostPreviewRowBig from './widgets/postPreview/PostPreviewRowBig'
+import InfoSidebar from './InfoSidebar'
+import PostPreviewCol from './widgets/postPreview/PostPreviewCol'
+
+
+const MoreArticles = () => {
+  let testRowArr = Array.from({ length:6 })
+  let testBottomArr = Array.from({ length:4 })
+
+  return (
+    <div className='bg-white my-12'>
+        <div className='min-[995px]:grid min-[995px]:grid-cols-3 flex flex-col gap-6 '>
+            {/* main articles */}
+            <div className='col-span-2 '>
+            {testRowArr.map((el, index) => {
+                    return (
+                        <PostPreviewRowBig key={index} direction={"md:flex-row  flex-col"}/>
+                    )
+                })}
+            </div>
+            {/* info sidebar */}
+            <InfoSidebar/>
+        </div>
+        {/* bottom articles */}
+        <div className='min-[900px]:grid min-[900px]:grid-cols-4 gap-6 flex flex-col'>
+            {testBottomArr.map((el, index) => {
+                return (
+                    <PostPreviewCol key={index} {...{showDescription:true}}/>
+                )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default MoreArticles
