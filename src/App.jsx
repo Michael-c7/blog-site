f2dab334d56be6dd7ef0e8468aea6aada9af0b84
Michael Carr <mcarr2468@gmail.com>
2023-03-10 20:35:23 -0600

    adde the liked posts as a route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 
 import PrivateRoutes from './utility/PrivateRoutes'
@@ -23,8 +23,25 @@ import Sidebar from './components/Sidebar'
 import Overlay from './components/Overlay'
 import SearchOverlay from './components/SearchOverlay'
 import ScrollToTop from './components/ScrollToTop'
+import LikedPosts from './pages/LikedPosts';
 
 function App() {
+  const [wrapperHeight, setWrapperHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    function handleResize() {
+      setWrapperHeight(window.innerHeight);
+      console.log(window.innerHeight)
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+    // style={{ height: `${wrapperHeight}px` }}
+  }, []);
 
   return (
     <div className='site-wrapper'>
@@ -43,7 +60,7 @@ function App() {
         <Routes>
           {/* All private routes go in here */}
           <Route element={<PrivateRoutes/>}>
-            <Route element={<CreateAPost/>} path="/createapost"/>
+            <Route element={<CreateAPost/>} path="/createAPost"/>
           </Route>
 
           <Route element={<Home/>} path="/"/>
@@ -53,6 +70,7 @@ function App() {
           <Route element={<Category/>} path="/category/:categoryName"/>
           <Route element={<Post/>} path="/post/:postId"/>
           <Route element={<Search/>} path="/search"/>
+          <Route element={<LikedPosts/>} path="/likedPosts"/>
 
           <Route element={<Error/>} path="*"/>
         </Routes>
