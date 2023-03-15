ab22c18f6c35c23388fb12b449195817a4e01fe4
Michael Carr <mcarr2468@gmail.com>
2023-03-15 15:10:29 -0500

    fix layout position, created a moblie version and did some minor style improvments

--- src/pages/CreateAPost.jsx
+++ src/pages/CreateAPost.jsx
@@ -44,9 +44,9 @@ const CreateAPost = () => {
     - add different image [X]
     - get all data into createPost function [X]
 
-    - add uniqueId to post [DOING NOW]
-    - fix style to look good
-    - create mobile version
+    - add uniqueId to post [X]
+    - fix style to look good [X]
+    - create mobile version [X]
 
     - get real images from api(s)
 
@@ -172,116 +172,118 @@ const CreateAPost = () => {
   
 
   return (
-    <div className="">
+    <>
       <GeneralPageHeading text={"create a post"}/>
 
-      <div className="outer-width mx-auto my-0 flex flex-col justify-center relative">
+      <div className="outer-width mx-auto my-4 relative flex flex-col justify-center items-center">
         {/* image section */}
-        <div className="my-6 relative">
-          <h2 className="mb-2 font-semibold">Add an image</h2>
-          {isImagePreviewShown ? (
-            <div className="flex rounded-sm w-1/2 h-72 text-white">
-              <div className="absolute w-1/2 flex justify-end p-2">
-                <button className="text-2xl bg-gray-800 bg-opacity-75 w-10 h-10 rounded-full flex items-center justify-center" onClick={() => {
-                  // setCurrentImage("")
-                  setIsImagePreviewShown(false)
-                }}>
-                  <BiTrash/>
-                </button>
-              </div>
-              <img src={currentImage} alt={currentImage} className="object-cover w-full h-full rounded-sm"/>
-            </div>
-          ) : (
-            <div className=" bg-gray-100 p-4 flex rounded-sm w-1/2 border-2 border-dashed border-blue-500" >
-              <div className="bg-blue-600 text-white text-xl p-1 rounded-full w-10 h-10 flex items-center justify-center">
-                <MdOutlineAddPhotoAlternate/>
-              </div>
-              <div className="ml-4">
-                <h4 className="my-1">Add an image and see a preview of that image here</h4>
-                <button ref={openImageDropdownRef} onClick={() => setIsImageDropdownOpen(true)} className="bg-white font-semibold rounded-sm py-2 px-3 border border-gray-300">Add Image</button>
-              </div>
-            </div>
-          )}
-
-          {/* dropdown for the images */}
-          <div ref={imageDropdownMenuRef} className={`dropdown-menu-container ${isImageDropdownOpen ? "dropdown-menu-container--open" : "dropdown-menu-container--closed hidden"} left-0 w-1/2 h-72 grid grid-col-2 px-0`}>
-            <header className="flex items-center relative px-3">
-              {/* search image container */}
-              <div className="bg-slate-100 flex items-center p-2 w-full">
-                <div className="top-[2px] relative text-slate-500 text-lg mr-1">
-                  <BiSearch/>
+        <div className="flex flex-col justify-center md:w-[75%] w-full">
+          <div className="my-6 relative">
+            <h2 className="mb-2 font-semibold">Add an image</h2>
+            {isImagePreviewShown ? (
+              <div className="flex rounded-sm w-full h-[450px] text-white">
+                <div className="absolute w-full flex justify-end p-2">
+                  <button className="text-2xl bg-gray-800 bg-opacity-75 w-10 h-10 rounded-full flex items-center justify-center" onClick={() => {
+                    // setCurrentImage("")
+                    setIsImagePreviewShown(false)
+                  }}>
+                    <BiTrash/>
+                  </button>
                 </div>
-                <input className="bg-slate-100 w-full focus:outline-none focus:border-transparent" placeholder="search for an image" onChange={(e) => setSearchImageInput(e.target.value)} />
+                <img src={currentImage} alt={currentImage} className="object-cover w-full h-full rounded-sm"/>
               </div>
-              <div className="px-2">
-                {/* goes here */}
-                <select id="image-source" value={imageSourceSelect} onChange={(e) => setImageSourceSelect(e.target.value)}>
-                  <option value="unsplashed">Unsplashed</option>
-                  <option value="pexels">Pexels</option>
-                  <option value="default">Default</option>
-                </select>
+            ) : (
+              <div className="bg-gray-100 p-4 flex rounded-sm w-full border-2 border-dashed border-blue-500" >
+                <div className="bg-blue-600 text-white text-xl p-1 rounded-full w-10 h-10 flex items-center justify-center">
+                  <MdOutlineAddPhotoAlternate/>
+                </div>
+                <div className="ml-4">
+                  <h4 className="my-1">Add an image and see a preview of that image here</h4>
+                  <button ref={openImageDropdownRef} onClick={() => setIsImageDropdownOpen(true)} className="bg-white font-semibold rounded-sm py-2 px-3 border border-gray-300">Add Image</button>
+                </div>
               </div>
-              <button className="text-2xl text-slate-500 px-3" onClick={() => setIsImageDropdownOpen(false)}>
-                <RiCloseLine/>
-              </button>
-            </header>
-            <section className="my-4 h-full overflow-auto before:content-[''] before:absolute before:w-full before:h-[2px] before:bg-slate-100 before:left-0">
-              <div className="grid grid-cols-2 gap-3 h-full px-3 ">
-                {dropdownImages.map((el, index) => {
+            )}
 
-                  return (
-                    <button key={index} className="" onClick={(e) => {
-                      chooseImage(e.target)
-                    }}>
-                      <img src={el.src} alt="alt text here" />
-                    </button>
-                  )
-                })}
+            {/* dropdown for the images */}
+            <div ref={imageDropdownMenuRef} className={`dropdown-menu-container ${isImageDropdownOpen ? "dropdown-menu-container--open" : "dropdown-menu-container--closed hidden"} left-0 md:w-[70%] w-full  h-72 grid grid-col-2 px-0`}>
+              <header className="flex min-[376px]:flex-row flex-col items-center relative px-3">
+                {/* search image container */}
+                <div className="bg-slate-100 flex items-center p-2 w-full">
+                  <div className="top-[2px] relative text-slate-500 text-lg mr-1">
+                    <BiSearch/>
+                  </div>
+                  <input className="bg-slate-100 w-full focus:outline-none focus:border-transparent" placeholder="search for an image" onChange={(e) => setSearchImageInput(e.target.value)} />
+                </div>
+                <div className="px-2 min-[376px]:py-0 py-6 ">
+                  {/* goes here */}
+                  <select id="image-source" value={imageSourceSelect} onChange={(e) => setImageSourceSelect(e.target.value)}>
+                    <option value="unsplashed">Unsplashed</option>
+                    <option value="pexels">Pexels</option>
+                    <option value="default">Default</option>
+                  </select>
+                </div>
+                <button className="text-2xl px-3 bg-gray-100 min-[376px]:text-slate-500 text-slate-700 min-[376px]:bg-transparent" onClick={() => setIsImageDropdownOpen(false)}>
+                  <RiCloseLine/>
+                </button>
+              </header>
+              <section className="my-4 h-full overflow-auto before:content-[''] before:absolute before:w-full before:h-[2px] before:bg-slate-100 before:left-0">
+                <div className="grid grid-cols-2 gap-3 h-full px-3 ">
+                  {dropdownImages.map((el, index) => {
+                    return (
+                      <button key={index} className=" bg-orange-500" onClick={(e) => {
+                        chooseImage(e.target)
+                      }}>
+                        <img src={el.src} alt="alt text here" className="w-full h-full object-cover" />
+                      </button>
+                    )
+                  })}
+                </div>
+              </section>
+            </div> 
+            <div className={`mt-4 flex items-center ${isImagePreviewShown ? "justify-between" : "justify-end"}  w-full`}>
+                {isImagePreviewShown ? (
+                  <button ref={imageDropdownMenuRef}  onClick={() => setIsImageDropdownOpen(true)} className="bg-white font-semibold rounded-sm py-2 px-3 border border-gray-300">Add Different Image</button>
+                ) : ""}
+                <p className="">{imageSourceSelectText}</p>
               </div>
-            </section>
-          </div> 
-          <div className={`mt-4 flex items-center ${isImagePreviewShown ? "justify-between" : "justify-end"}  w-1/2`}>
-              {isImagePreviewShown ? (
-                <button ref={imageDropdownMenuRef}  onClick={() => setIsImageDropdownOpen(true)} className="bg-white font-semibold rounded-sm py-2 px-3 border border-gray-300">Add Different Image</button>
-              ) : ""}
-              <p className="">{imageSourceSelectText}</p>
-            </div>
-        </div>
-        {/* post title */}
-        <div className="my-6">
-          <h2 className="mb-2 font-semibold ">Add title</h2>
-          <input className="w-1/2 p-2 rounded-sm border border-gray-300" value={postTitle} maxLength={postTitleMaxChar} onChange={(e) => setPostTitle(e.target.value)}/>
-          <p className="mt-3 w-1/2 text-end">{postTitle.length} / {postTitleMaxChar}</p>
-        </div>
+          </div>
+          {/* post title */}
+          <div className="my-6">
+            <h2 className="mb-2 font-semibold ">Add title</h2>
+            <input className="w-full p-2 rounded-sm border border-gray-300" value={postTitle} maxLength={postTitleMaxChar} onChange={(e) => setPostTitle(e.target.value)}/>
+            <p className="mt-3 w-full text-end">{postTitle.length} / {postTitleMaxChar}</p>
+          </div>
 
-        {/* select a tag for the post */}
-        <div className="flex flex-col my-6">
-          <label htmlFor="tags" className="mb-2 font-semibold">Select a tag:</label>
-          <select id="tags" name="tags" className="w-1/2 p-2 rounded-sm border border-gray-300" value={postTag} onChange={(e) => setPostTag(e.target.value)}>
-            <option value="">--Select a tag--</option>
-            <option value="science">Science</option>
-            <option value="gaming">Gaming</option>
-            <option value="business">Business</option>
-            <option value="movies">Movies</option>
-            <option value="food">Food</option>
-          </select>
-        </div>
+          {/* select a tag for the post */}
+          <div className="flex flex-col my-6">
+            <label htmlFor="tags" className="mb-2 font-semibold">Select a tag:</label>
+            <select id="tags" name="tags" className="w-full p-2 rounded-sm border border-gray-300" value={postTag} onChange={(e) => setPostTag(e.target.value)}>
+              <option value="">--Select a tag--</option>
+              <option value="science">Science</option>
+              <option value="gaming">Gaming</option>
+              <option value="business">Business</option>
+              <option value="movies">Movies</option>
+              <option value="food">Food</option>
+            </select>
+          </div>
 
 
-        {/* post text */}
-        <div className="my-6">
-          <h2 className="mb-2  font-semibold">Add text</h2>
-          <textarea className="w-1/2 h-72 p-2 rounded-sm border border-gray-300" value={postText} onChange={(e) => setPostText(e.target.value)} placeholder="Write your story here..." maxLength={postTextMaxChar}></textarea>
+          {/* post text */}
+          <div className="my-6">
+            <h2 className="mb-2  font-semibold">Add text</h2>
+            <textarea className="w-full h-72 p-2 rounded-sm border border-gray-300" value={postText} onChange={(e) => setPostText(e.target.value)} placeholder="Write your story here..." maxLength={postTextMaxChar}></textarea>
+          </div>
+
+          {/* publish / create the post */}
+          <div className="mb-6 flex justify-between w-full">
+            {/* <button className="bg-white text-black border-2 border-black rounded-sm py-2 px-3">Cancel</button> */}
+            <p>{postText.length} / {postTextMaxChar}</p>
+            <button className="bg-black text-white rounded-sm py-2 px-3 disabled:opacity-75 disabled:bg-slate-700" type="button" disabled={isImagePreviewShown && currentImage && postTitle && postTag && postText ? false : true} onClick={() => createPost()}>Create Post</button>
+          </div>
         </div>
 
-        {/* publish / create the post */}
-        <div className="mb-6 flex justify-between w-1/2">
-          {/* <button className="bg-white text-black border-2 border-black rounded-sm py-2 px-3">Cancel</button> */}
-          <p>{postText.length} / {postTextMaxChar}</p>
-          <button className="bg-black text-white rounded-sm py-2 px-3 disabled:opacity-75 disabled:bg-slate-700" type="button" disabled={isImagePreviewShown && currentImage && postTitle && postTag && postText ? false : true} onClick={() => createPost()}>Create Post</button>
-        </div>
       </div>
-    </div>
+    </>
   )
 }
 
