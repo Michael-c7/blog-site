476818b8609f85607202df27373a5ad3b0be5bda
Michael Carr <mcarr2468@gmail.com>
2023-03-24 10:59:47 -0500

    did the style for search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import GeneralPageComponent from '../components/GeneralPageComponent'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <>
+      <div className='outer-width mx-auto mt-10'>
+        <p className='min-[320px]:text-4xl text-2xl capitalize font-semibold text-gray-500'>results for 
+          {/* the search query text goes here */}
+          <span className='text-gray-900'> games</span>
+        </p>
+      </div>
+      <GeneralPageComponent {...{isGeneralHeadingShown:false}}/>
+    </>
   )
 }
 
