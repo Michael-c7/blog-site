6682374297b167ede9b290ea01f197dd2fc3e546
Michael Carr <mcarr2468@gmail.com>
2023-03-27 08:44:29 -0500

    refactored the background of TopOfTheWeek

--- src/components/TopOfTheWeek.jsx
+++ src/components/TopOfTheWeek.jsx
@@ -13,40 +13,42 @@ const TopOfTheWeek = () => {
   let testSideArr = Array.from({ length:4 })
 
   return (
-    <article className='top-of-the-week--before relative py-10'>
-      <h2 className="text-3xl font-medium mb-6">Top of the week</h2>
-      <div>
-        <div className='min-[995px]:grid min-[995px]:grid-cols-3 gap-6 flex flex-col mb-12 '>
-          {/**main article here */}
-          <section className='relative col-span-2'>
-            <Link to="/to post here">
-              <img src={testImg} alt="the alt text here" title="alt text here" className='w-full h-full rounded-xl'/>
-            </Link>
-            <div className='absolute bottom-0 m-6'>
-              <Tag {...{bgColor:"#ccc", link:"/test", text:"tag text"}}/>
-              <h2 className='text-white font-medium md:text-3xl text-2xl my-2'>the title goes here</h2>
-              <AuthorAndDate {...{textColor:"#fff"}}/>
+    <div className="bg-[#FDEEDC]">
+      <article className='top-of-the-week--before relative py-10 outer-width mx-auto'>
+        <h2 className="text-3xl font-medium mb-6">Top of the week</h2>
+        <div>
+          <div className='min-[995px]:grid min-[995px]:grid-cols-3 gap-6 flex flex-col mb-12 '>
+            {/**main article here */}
+            <section className='relative col-span-2'>
+              <Link to="/to post here">
+                <img src={testImg} alt="the alt text here" title="alt text here" className='w-full h-full rounded-xl'/>
+              </Link>
+              <div className='absolute bottom-0 m-6'>
+                <Tag {...{bgColor:"#ccc", link:"/test", text:"tag text"}}/>
+                <h2 className='text-white font-medium md:text-3xl text-2xl my-2'>the title goes here</h2>
+                <AuthorAndDate {...{textColor:"#fff"}}/>
+              </div>
+            </section>
+            {/* sidebar articles here */}
+            <div className='grid grid-rows-4 gap-6'>
+              {testSideArr.map((el, index) => {
+                return (
+                  <PostPreviewRow key={index}/>
+                  )
+                })}
             </div>
-          </section>
-          {/* sidebar articles here */}
-          <div className='grid grid-rows-4 gap-6'>
-            {testSideArr.map((el, index) => {
+          </div>
+          {/* bottom together articles here */}
+          <div className='grid min-[900px]:grid-cols-3 grid-cols-1 gap-6'>
+            {testBottomArr.map((el, index) => {
               return (
-                <PostPreviewRow key={index}/>
+                <PostPreviewTogether key={index}/>
                 )
               })}
           </div>
         </div>
-        {/* bottom together articles here */}
-        <div className='grid min-[900px]:grid-cols-3 grid-cols-1 gap-6'>
-          {testBottomArr.map((el, index) => {
-            return (
-              <PostPreviewTogether key={index}/>
-              )
-            })}
-        </div>
-      </div>
-    </article>
+      </article>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,3 @@
-import React, { useState, useEffect, useRef } from 'react'
-
-
 // components
 import HeroSlider from '../components/HeroSlider'
 import EditorsChoice from '../components/EditorsChoice'
@@ -10,12 +7,21 @@ import MoreArticles from '../components/MoreArticles'
 
 const Home = () => {
   return (
-    <div className='outer-width mx-auto'>
-      <HeroSlider/>
-      <EditorsChoice/>
+    <>
+      <div className='outer-width mx-auto'>
+        <HeroSlider/>
+      </div>
+
+      <div className='outer-width mx-auto'>
+        <EditorsChoice/>
+      </div>
+
       <TopOfTheWeek/>
-      <MoreArticles/>
-    </div>
+
+      <div className='outer-width mx-auto'>
+        <MoreArticles/>
+      </div>
+    </>
   )
 }
 
