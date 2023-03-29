e91c8a129577b119dc058a104cf0a77643443865
Michael Carr <mcarr2468@gmail.com>
2023-03-29 16:04:57 -0500

    replaced the double quote in the editor choice text with a single quote

--- src/components/EditorsChoice.jsx
+++ src/components/EditorsChoice.jsx
@@ -9,7 +9,7 @@ let testArr = Array.from({ length:6 })
 const EditorsChoice = () => {
   return (
     <article className="my-8">
-        <h2 className="text-3xl font-medium mb-8">Editor"s Choice</h2>
+        <h2 className="text-3xl font-medium mb-8">Editor's Choice</h2>
         {/* 3x3 grid goes here */}
         <div className="grid min-[900px]:grid-cols-3 grid-cols-31 gap-6">
           {testArr.map((el, index) => {
