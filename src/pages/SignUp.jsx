c80fdca1e10afaa070b17e6e601bb9a227f7a9d6
Michael Carr <mcarr2468@gmail.com>
2023-04-01 09:20:33 -0500

    added css for username availability and some boilerplate code for the submit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,28 +1,52 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import GeneralHeading from '../components/GeneralHeading'
+import { GrCheckmark, GrClose } from "react-icons/gr"
+
 
 const SignUp = () => {
   const [signUpStateData, setSignUpStateData] = React.useState({
-    email:'',
-    password:'',
+    username:"",
+    displayName:"",
+    email:"",
+    password:"",
   })
 
-  React.useEffect(() => {
+  const onSubmit = e => {
+    e.preventDefault()
+    // submit data to firebase here
     console.log(signUpStateData)
-  }, [signUpStateData])
+  }
+
+  // React.useEffect(() => {
+  //   console.log(signUpStateData)
+  // }, [signUpStateData])
+
+
+  // temp var
+  let isUsernameAvailable = true
+
+  const isSubmitAllowed = (
+    signUpStateData.username
+    && isUsernameAvailable 
+    && signUpStateData.displayName
+    && signUpStateData.email
+    && signUpStateData.password
+  )
 
   return (
     <div className="flex flex-col">
       <GeneralHeading text={"Sign up"}/>
-      <form className="form-card">
+      <form className="form-card" onSubmit={onSubmit}>
         {/* username is the unique address for the user eg: author/@johnSmith */}
         <div className="form-input-container">
           <label className="form-label" htmlFor="login-username">Username</label>
           <input className="form-input" name="login-username" id="login-username" type="text" placeholder="eg: johnSmith123abc" onChange={(e) => setSignUpStateData({...signUpStateData, username:e.target.value})}/>
-          <p>@john123abc is available</p>
+          {signUpStateData.username ? (
+          <p className={`${isUsernameAvailable ? "text-green-600" : "text-red-600"}`}>@{signUpStateData.username} {isUsernameAvailable ? " is available" : " is already taken"}</p>
+          ) : ""}
         </div>
-        {/* display name is whats in the author eg: john smith */}
+        {/* display name is not unique and whats in the author eg: john smith */}
         <div className="form-input-container">
           <label className="form-label" htmlFor="login-displayname">Display name</label>
           <input className="form-input" name="login-displayname" id="login-displayname" type="text" placeholder="eg: john smith" onChange={(e) => setSignUpStateData({...signUpStateData, displayName:e.target.value})}/>
@@ -37,7 +61,7 @@ const SignUp = () => {
         </div>
         <p className='text-center'>Have an account? <Link className="text-blue-500"  to="/login">Login</Link></p>
         
-        <button className="form-primary-btn" type="submit">Sign Up</button>
+        <button className={`form-primary-btn  ${isSubmitAllowed ? "opacity-100" : " opacity-50"}`} disabled={isSubmitAllowed ? false : true} type="submit">Sign Up</button>
       </form>
     </div>
   )
