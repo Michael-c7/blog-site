92eb0a90aea07ce0dca22ec2c1d52e5f7969e68b
Michael Carr <mcarr2468@gmail.com>
2023-04-03 11:35:28 -0500

    added the logic for the login page functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import GeneralHeading from '../components/GeneralHeading'
+import { useAuthContext } from "../Auth/AuthContext"
 
 const Login = () => {
+  const { signInUser } = useAuthContext()
+  const navigate = useNavigate();
+
   const [loginStateData, setLoginStateData] = React.useState({
     email:'',
     password:'',
   })
 
-  React.useEffect(() => {
-    console.log(loginStateData)
-  }, [loginStateData])
+  const isSubmitAllowed = (
+    loginStateData.email
+    && loginStateData.password
+  )
+
+
+  const onSubmit = e => {
+    e.preventDefault()
+    
+    if(isSubmitAllowed) {
+      signInUser(loginStateData.email, loginStateData.password)
+      navigate("/")
+    }
+  }
+
+  
 
   return (
     <div className="flex flex-col">
       <GeneralHeading/>
-      <form className="form-card">
+      <form className="form-card" onSubmit={onSubmit}>
         <div className="form-input-container">
           <label className="form-label" htmlFor="login-email">Email Address</label>
           <input className="form-input" name="login-email" id="login-email" type="email" onChange={(e) => setLoginStateData({...loginStateData, email:e.target.value})}/>
@@ -24,8 +41,8 @@ const Login = () => {
           <label className="form-label" htmlFor="login-password">Password</label>
           <input className="form-input" name="login-password" id="login-password" type="password" onChange={(e) => setLoginStateData({...loginStateData, password:e.target.value})}/>
         </div>
-        <p className='text-center'>Don't have an account? <Link className="text-blue-500"  to="/signUp">Sign Up</Link></p>
-        <button className="form-primary-btn" type="submit">Login</button>
+        <p className="text-center">Don't have an account? <Link className="text-blue-500"  to="/signUp">Sign Up</Link></p>
+        <button className={`form-primary-btn ${isSubmitAllowed ? "opacity-100" : " opacity-50"}`} disabled={isSubmitAllowed ? false : true} type="submit">Login</button>
       </form>
     </div>
   )
