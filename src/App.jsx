8f45460074a9b121ea98cbc7369b5850246bd313
Michael Carr <mcarr2468@gmail.com>
2023-04-03 19:58:10 -0500

    created a route for the forogotpassword page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
-import PrivateRoutes from './utility/PrivateRoutes'
+import PrivateRoutes from "./utility/PrivateRoutes"
 
 // public route
-import Home from './pages/Home'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import Author from './pages/Author'
-import Category from './pages/Category'
-import Post from './pages/Post'
-import Search from './pages/Search'
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/forgotPassword"
+import Author from "./pages/Author"
+import Category from "./pages/Category"
+import Post from "./pages/Post"
+import Search from "./pages/Search"
 
-import Error from './pages/Error'
+import Error from "./pages/Error"
 // private route
-import CreateAPost from './pages/CreateAPost'
+import CreateAPost from "./pages/CreateAPost"
 
 // component
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Sidebar from './components/Sidebar'
-import Overlay from './components/Overlay'
-import SearchOverlay from './components/SearchOverlay'
-import ScrollToTop from './components/ScrollToTop'
-import LikedPosts from './pages/LikedPosts';
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Sidebar from "./components/Sidebar"
+import Overlay from "./components/Overlay"
+import SearchOverlay from "./components/SearchOverlay"
+import ScrollToTop from "./components/ScrollToTop"
+import LikedPosts from "./pages/LikedPosts";
 
 function App() {
 
@@ -50,6 +51,7 @@ function App() {
           <Route element={<Home/>} path="/"/>
           <Route element={<Login/>} path="/login"/>
           <Route element={<SignUp/>} path="/SignUp"/>
+          <Route element={<ForgotPassword/>} path="/forgotPassword"/>
           <Route element={<Author/>} path="/author/:authorId"/>
           <Route element={<Category/>} path="/category/:categoryName"/>
           <Route element={<Post/>} path="/post/:postId"/>
