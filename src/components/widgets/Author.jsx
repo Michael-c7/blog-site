5b5a41b5855c7e6d965561c17a1fc93f2234b493
Michael Carr <mcarr2468@gmail.com>
2023-04-15 17:41:48 -0500

    added authorLink props and added use of authorName and authorImg props

--- src/components/widgets/Author.jsx
+++ src/components/widgets/Author.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import defaultAuthorImg from "../../assets/images/testAuthorImg1.jpg"
+import defaultUserImg from "../../assets/images/defaultUser.png" 
 
 const Author = (props) => {
-    const { authorImg, authorName, textColor, fontSize, imageSize } = props
+    const { authorImg, authorName, authorLink, textColor, fontSize, imageSize } = props
 
     return (
-        <Link to="/ to author page" className='flex flex-row items-center text-gray-500'>
-            <img className='w-6 h-6 rounded-full object-cover' src={defaultAuthorImg} alt={`author img alt text`}/>
-            <h2 className="text-sm min-[425px]:ml-2 ml-1 mr-3" style={{color:`${textColor}`}}>john smith</h2>
+        <Link to={authorLink} className="flex flex-row items-center text-gray-500">
+            <img className="w-6 h-6 rounded-full object-cover" src={authorImg ? authorImg : defaultUserImg} alt={`author img alt text`}/>
+            <h2 className="text-sm min-[425px]:ml-2 ml-1 mr-3" style={{color:`${textColor}`}}>{authorName ? authorName : "author name"}</h2>
         </Link>
     )
 }
