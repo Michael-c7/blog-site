4d1c0aeead506779f35f76df285a724ef622119a
Michael Carr <mcarr2468@gmail.com>
2023-04-15 17:29:36 -0500

    added logic to determine if the bgColor is a css variable or not and applies styles accordingly

--- src/components/widgets/Tag.jsx
+++ src/components/widgets/Tag.jsx
@@ -10,7 +10,7 @@ const Tag = (props) => {
   const { bgColor, link, text } = props
   
   return (
-    <Link to={link} className={`text-xs py-1 px-4 font-medium rounded uppercase text-white text-center`} style={{backgroundColor:`${bgColor}`}}>
+    <Link to={link} className={`text-xs py-1 px-4 font-medium rounded uppercase text-white text-center`} style={{backgroundColor:`${bgColor.slice(0, 2) === "--" ? `var(${bgColor})` : `${bgColor}`}`}}>
     {text}
     </Link>
   )
