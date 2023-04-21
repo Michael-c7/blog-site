166e26c9cebf20117c44df24681bca1f94bb6eed
Michael Carr <mcarr2468@gmail.com>
2023-04-21 15:55:03 -0500

    added text prop for custom error messages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Error = () => {
+const Error = (props) => {
   return (
     <div className="h-[75vh] flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-semibold">An Error occurred.</h1>
+      <h1 className="text-3xl font-semibold">{props.text ? props.text : "An Error occurred."}</h1>
       <Link to="/">
         <button className="form-primary-btn" type="button">Go Home</button>
       </Link>
