8816eef3c9c8813e71b45b91ae4594a0a549a922
Michael Carr <mcarr2468@gmail.com>
2023-05-15 20:33:38 -0500

    get the data for the page from the database

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 import GeneralPageComponent from '../components/GeneralPageComponent'
 
+import { useBlogContext } from '../contexts/blog_context'
+
 const Author = () => {
+  const { 
+    paginatedBlogPosts,
+    POSTS_PER_PAGE,
+    currentGeneralPagePosts,
+    paginationDotsLoaded,
+    setPaginationDotsLoaded,
+    getPostsByProperty,
+    currentDisplayName,
+  } = useBlogContext()
+
+  
+  let [currentPageNumber, setCurrentPageNumber] = React.useState(1)
+  let paginationDotAmount = Math.ceil(paginatedBlogPosts.length / POSTS_PER_PAGE)
+
+
+  useEffect(() => {
+    getPostsByProperty("username", "scifacts", currentPageNumber)
+  }, [currentPageNumber])
+
+  useEffect(() => {
+    // reset so if we go to another will still work
+    if(paginationDotsLoaded) {
+      setPaginationDotsLoaded(false)
+    }
+  }, [paginationDotsLoaded])
+
   return (
     <>
-      <GeneralPageComponent {...{headingText:"test author name"}}/>
+      <GeneralPageComponent {...{headingText:currentDisplayName ? currentDisplayName : "name unknown", paginationDotAmount, currentPageNumber, setCurrentPageNumber, currentGeneralPagePosts }}/>
     </>
   )
 }
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GeneralPageComponent from '../components/GeneralPageComponent'
 
+import { useBlogContext } from '../contexts/blog_context'
+
+import { useParams } from 'react-router-dom';
+
+
 const Category = () => {
+  const { 
+    paginatedBlogPosts,
+    POSTS_PER_PAGE,
+    currentGeneralPagePosts,
+    paginationDotsLoaded,
+    setPaginationDotsLoaded,
+    getPostsByProperty,
+  } = useBlogContext()
+
+  const { categoryName } = useParams();
+
+  let [currentPageNumber, setCurrentPageNumber] = React.useState(1)
+  let paginationDotAmount = Math.ceil(paginatedBlogPosts.length / POSTS_PER_PAGE)
+  let [currentCategory, setCurrentCategory] = useState(null)
+
+
+  useEffect(() => {
+    setCurrentCategory(categoryName)
+  }, [])
+
+
+  useEffect(() => {
+    getPostsByProperty("tag", currentCategory, currentPageNumber)
+  }, [currentCategory, currentPageNumber])
+
+  useEffect(() => {
+    // reset so if we go to another will still work
+    if(paginationDotsLoaded) {
+      setPaginationDotsLoaded(false)
+    }
+  }, [paginationDotsLoaded])
+
+
   return (
     <>
-      <GeneralPageComponent {...{headingText:"test category posts"}}/>
+      <GeneralPageComponent {...{headingText:currentCategory ? currentCategory : "name unknown", paginationDotAmount, currentPageNumber, setCurrentPageNumber, currentGeneralPagePosts }}/>
     </>
   )
 }
