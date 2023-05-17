fc377cdff57ca03b4b17bcf3ea8218650cc1a6cd
Michael Carr <mcarr2468@gmail.com>
2023-05-17 11:48:48 -0500

    did some work on the search

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -2,12 +2,20 @@ import React, { useState, useEffect, useRef } from "react"
 
 import { VscChromeClose } from "react-icons/vsc"
 import { useStandardContext } from "../contexts/standard_context";
+import { useBlogContext } from "../contexts/blog_context";
+
 import useGetScrollY from "../hooks/useGetScrollY"
+import { redirect } from "react-router-dom";
+
 
 const SearchOverlay = () => {
-    const { isSidebarOverlayOpen, closeSearchOverlay  } = useStandardContext()
+    const { isSidebarOverlayOpen, closeSearchOverlay } = useStandardContext()
+    const { setCurrentSearchTerm } = useBlogContext()
 
     const { scrollY } = useGetScrollY()
+
+    
+    let [localSearchTerm, setLocalSearchTerm] = useState("")
 
 
     React.useEffect(() => {
@@ -18,13 +26,33 @@ const SearchOverlay = () => {
         }
     },[isSidebarOverlayOpen])
 
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            // Enter key was pressed
+            performSearch()
+            // close the search overlay
+            closeSearchOverlay()
+            // redirect to the /search page
+            return redirect("/search")
+        }
+    }
+
+    const performSearch = (event) => {
+        // Perform search logic here using the search term
+        // the actual search term
+        setCurrentSearchTerm(localSearchTerm)
+        // Reset the local search term
+        setLocalSearchTerm("")
+      }
+
     return (
         <div className={`absolute w-full h-full top-0 bg-gray-100 ${isSidebarOverlayOpen ? "opacity-80 z-50 block" : "opacity-0 -z-50 hidden"}`} style={{top:`${scrollY}px`}}>
             <button className="absolute text-3xl top-0 right-0 m-8 p-3 z-10" onClick={() => closeSearchOverlay()}>
                 <VscChromeClose/>
             </button>
-            <div className="absolute w-full h-full flex justify-center items-center ">
-             <input type="text" placeholder="Search..." className="absolute bg-transparent placeholder:text-black text-6xl font-medium max-[850px]:text-4xl max-[500px]:text-3xl max-[425px]:w-11/12 border-b-2 border-black"/> 
+            <div className="absolute w-full h-full flex justify-center items-center">
+             <input type="text" placeholder="Search..." value={localSearchTerm} className="absolute bg-transparent placeholder:text-black text-6xl font-medium max-[850px]:text-4xl max-[500px]:text-3xl max-[425px]:w-11/12 border-b-2 border-black" onChange={(event) => setLocalSearchTerm(event.target.value)} onKeyDown={handleKeyDown}/> 
             </div>
         </div>
     )
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GeneralPageComponent from '../components/GeneralPageComponent'
 
+import { useBlogContext } from '../contexts/blog_context'
+
 const Search = () => {
+  const { 
+    paginatedBlogPosts,
+    POSTS_PER_PAGE,
+    currentGeneralPagePosts,
+    paginationDotsLoaded,
+    setPaginationDotsLoaded,
+    getPostsByProperty,
+    currentSearchTerm,
+    getSearchPosts,
+  } = useBlogContext()
+
+  let [currentPageNumber, setCurrentPageNumber] = React.useState(1)
+  let paginationDotAmount = Math.ceil(paginatedBlogPosts.length / POSTS_PER_PAGE)
+
+  // the history and evolution of Biryani
+  useEffect(() => {
+    if(currentSearchTerm) {
+      getSearchPosts(currentSearchTerm, currentPageNumber)
+    }
+  }, [currentSearchTerm, currentPageNumber])
+
+  useEffect(() => {
+    // reset so if we go to another will still work
+    if(paginationDotsLoaded) {
+      setPaginationDotsLoaded(false)
+    }
+  }, [paginationDotsLoaded])
+
   return (
     <>
       <div className='outer-width mx-auto mt-10'>
         <p className='min-[320px]:text-4xl text-2xl capitalize font-semibold text-gray-500'>results for 
           {/* the search query text goes here */}
-          <span className='text-gray-900'> games</span>
+          <span className='text-gray-900'> {currentSearchTerm}</span>
         </p>
       </div>
-      <GeneralPageComponent {...{isGeneralHeadingShown:false}}/>
+      <GeneralPageComponent {...{isGeneralHeadingShown:false, paginationDotAmount, currentPageNumber, setCurrentPageNumber, currentGeneralPagePosts }}/>
     </>
   )
 }
