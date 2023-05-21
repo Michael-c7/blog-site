782e11e149d78ec98dae5b3fc0460f988b6c10bb
Michael Carr <mcarr2468@gmail.com>
2023-05-20 19:49:25 -0500

    allow live data to be used

--- src/components/widgets/postPreview/PostPreviewRow.jsx
+++ src/components/widgets/postPreview/PostPreviewRow.jsx
@@ -3,6 +3,7 @@ import Tag from "../Tag"
 import Date from "../Date"
 import testImg from "../../../assets/images/testImg1.jpg"
 import { Link } from "react-router-dom"
+import { getDateFromTime } from "../../../utility/misc"
 
 /*
 Where image is on the left
@@ -11,6 +12,7 @@ are on the right
 
 have a reverse prop for reverse needs eg:recent posts
 */
+
 
 /**
  * 
@@ -21,18 +23,27 @@ have a reverse prop for reverse needs eg:recent posts
  */
 const PostPreviewRow = (props) => {
   // ${props.direction === "row-reverse" ? "flex-row-reverse" : "flex-row"}
+
+  let [data, setData] = React.useState([])
+
+  React.useEffect(() => {
+    setData(props.post)
+  },[props])
+
+  let titleTitleCutoff = 45;
+
   return (
     <div className={`flex items-center  ${props.direction ? props.direction : "flex-row"} ${props.direction === "flex-row-reverse" ? "justify-between" : ""}`}>
         <Link className="w-24 h-24 flex-shrink-0" to="/link to post">
-          <img src={testImg} alt={"alt text here"} title={`alt text here`} className="w-full h-full rounded-xl object-cover  "/>
+          <img src={data?.image ? data?.image : testImg} alt={data?.title} title={data?.title} className="w-full h-full rounded-xl object-cover  "/>
         </Link>
 
       <div className={`${props.direction === "flex-row-reverse" ? "ml-0 mr-3" : "ml-3 mr-0"}`}>
-        <Tag {...{bgColor:"#ccc", link:"/test", text:"tag text"}}/>
-        <Link to="/link to post here">
-          <h2 className="font-bold text-black my-2 leading-snug	" style={{color:props.textColor}}>The Great Time For Enjoy City View On Mountain</h2>
+        <Tag {...{bgColor:`--category--${data?.tag}`, link:`/category/${data?.tag}`, text:`${data?.tag}`}}/>
+        <Link to={`/post/${data?.postId}`}>
+          <h2 className="font-bold text-black my-2 leading-snug" style={{color:props.textColor}}>{data?.title ? (data.title.length > titleTitleCutoff ? `${data.title.slice(0, titleTitleCutoff).trim()}...` : data?.title) : "no title"}</h2>
         </Link>
-        <Date/>
+        <Date {...{date:getDateFromTime(data?.createdAt?.nanoseconds, data?.createdAt?.seconds)}}/>
       </div>
     </div>
   )
